37c09f81e274e775e0a95874711a7fb298cb41d1
Глеб Останин <gleb.ostanin2014@yandex.ru>
2023-08-04 15:42:36 +0600

    Added form for adding tasks

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render, redirect
-from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import TaskForm
 from .models import Task
